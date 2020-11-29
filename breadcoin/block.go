f4e3c550797159476e974eb4ad5f57e193a3d219
Stan He <49423142+StanBHe@users.noreply.github.com>
2020-11-28 21:27:12 -0800

    Finish most block methods

--- breadcoin/block.go
+++ breadcoin/block.go
@@ -25,8 +25,12 @@ func (b Block) hashVal() []byte {
 	return utils.Hash(b.serialize())
 }
 
-func (b Block) totalRewards() int {
-	return b.CoinbaseReward
+func (b Block) totalRewards() float64 {
+	total := float64(b.CoinbaseReward)
+	for _, output := range b.Transactions {
+		total += output.Fee
+	}
+	return total
 }
 
 func (b Block) balanceOf(addr string) float64 {
@@ -56,12 +60,89 @@ func (b Block) serialize() string {
 	return string(jsonByte)
 }
 
+//IMPLEMENT
+func toJson() {
+}
+
 func (b Block) id() []byte {
 	return b.hashVal()
 }
 
-//UNOKENTNAEOITG! IMPLEMENT!!!!!!!!!!!!!!!!!!
-func addTransaction(tx Transaction, client int) bool {
+func (b Block) contains(tx Transaction) bool {
+	_, found := b.Transactions[string(tx.Id())]
+	if found {
+		return true
+	}
+	return false
+}
+
+//ONCE CLIENT IS DONE CHANGE THIS!!!!!!!!!!!!!!!!!
+func (b *Block) addTransaction(tx Transaction, client int) bool {
+	if _, found := b.Transactions[string(tx.Id())]; found {
+		fmt.Println(string(tx.Id()) + " is a duplicate")
+		return false
+	} else if tx.Sig == nil {
+		fmt.Println(string(tx.Id()) + " is unsigned")
+		return false
+	} else if !tx.ValidSignature() {
+		fmt.Println(string(tx.Id()) + " has an invalid signature")
+		return false
+	} else if !tx.SufficientFunds(*b) {
+		fmt.Println(string(tx.Id()) + " not enough gold for this transactions")
+		return false
+	}
+	nonce := 0
+	if val, found := b.NextNonce[string(tx.Id())]; found {
+		nonce = val
+	}
+
+	if tx.Nonce < nonce {
+		fmt.Println(string(tx.Id()) + " is replayed")
+		return false
+	} else if tx.Nonce > nonce {
+		fmt.Println(string(tx.Id()) + " out of order")
+		return false
+	} else {
+		b.NextNonce[tx.From] = nonce + 1
+	}
+
+	b.Transactions[string(tx.Id())] = tx
+
+	senderBalance := b.balanceOf(tx.From)
+	b.Balances[tx.From] = senderBalance - tx.TotalOutput()
+
+	for _, output := range tx.Outputs {
+		oldBalance := b.balanceOf(output.Addr)
+		b.Balances[output.Addr] = output.Amount + oldBalance
+	}
+
+	return true
+}
+
+func (b *Block) rerun(prevBlock *Block) bool {
+	b.Balances = make(map[string]float64)
+	b.NextNonce = make(map[string]int)
+	txs := b.Transactions
+	b.Transactions = make(map[string]Transaction)
+	for index, element := range prevBlock.Balances {
+		b.Balances[index] = element
+	}
+	for index, element := range prevBlock.NextNonce {
+		b.NextNonce[index] = element
+	}
+
+	winnerBalance := b.balanceOf(prevBlock.RewardAddr)
+	if prevBlock.RewardAddr != "" {
+		b.Balances[prevBlock.RewardAddr] = winnerBalance + prevBlock.totalRewards()
+	}
+
+	for _, element := range txs {
+		success := b.addTransaction(element, 0)
+		if !success {
+			return false
+		}
+	}
+
 	return true
 }
 
