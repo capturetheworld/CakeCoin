11c4c8bcb0e778b9d99dffbe22042f7a893b4e27
Ian S <ian@Ians-MBP.hsd1.ca.comcast.net>
2020-12-12 05:06:03 -0800

    miner

---
+++ cakecoin/miner.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"crypto/rsa"
+	"fmt"
+
+	"./utils"
+	//"./emitter"
+)
+
+//Miner is a client but since there is no subclasses
+type Miner struct {
+	Name, Address                                            string
+	Nonce                                                    int
+	Net                                                      FakeNet
+	KeyPair                                                  *rsa.PrivateKey
+	PendingOutgoingTransactions, PendingReceivedTransactions map[string]*Transaction
+	Blocks                                                   map[string]*Block
+	PendingBlocks                                            map[string]*Block
+	LastBlock                                                *Block
+	LastConfirmedBlock                                       *Block
+	ReceiveBlock                                             *Block
+	CurrentBlock                                             *Block
+	MiningRounds                                             int
+}
+
+func newMiner(name string, Net FakeNet, startingBlock *Block, keyPair *rsa.PrivateKey) *Miner {
+	var m Miner
+	m.Net = Net
+	m.Name = name
+
+	if keyPair == nil {
+		m.KeyPair = utils.GenerateKeypair()
+	} else {
+		m.KeyPair = keyPair
+	}
+
+	m.Address = utils.CalculateAddress(&m.KeyPair.PublicKey)
+	m.Nonce = 0
+
+	m.PendingOutgoingTransactions = make(map[string]*Transaction)
+	m.PendingReceivedTransactions = make(map[string]*Transaction)
+	m.Blocks = make(map[string]*Block)
+	m.PendingBlocks = make(map[string]*Block)
+
+	if startingBlock != nil {
+		m.setGenesisBlock(startingBlock)
+	}
+
+	m.MiningRounds = NUM_ROUNDS_MINING
+
+	return &m
+
+}
+
+func (m Miner) setGenesisBlock(startingBlock *Block) {
+	if m.LastBlock != nil {
+		fmt.Printf("Cannot set starting block for existing blockchain.")
+	}
+
+	m.LastConfirmedBlock = startingBlock
+	m.LastBlock = startingBlock
+	m.Blocks[string(startingBlock.id())] = startingBlock
+}
+
+/**
+ * Starts listeners and begins mining.
+ */
+func (m Miner) initialize() {
+	m.startNewSearch()
+
+	this.on(Blockchain.START_MINING, this.findProof)
+	this.on(Blockchain.POST_TRANSACTION, this.addTransaction)
+
+	// setTimeout(() => this.emit(Blockchain.START_MINING), 0); //needs implementing
+}
+
+func (m Miner) startNewSearch() {
+	m.currentBlock = Blockchain.makeBlock(m.address, m.lastBlock)
+
+	// txSet.forEach((tx) => this.addTransaction(tx));
+
+	// Start looking for a proof at 0.
+	this.currentBlock.proof = 0
+}
+
+//no optional parameters
+func (m Miner) findProof(oneAndDone bool) {
+	if(oneAndDone == nil){
+		oneAndDone = false
+	}
+
+	var pausePoint int = m.CurrentBlock.Proof + m.MiningRounds;
+    while (m.CurrentBlock.Proof < pausePoint) {
+      if (m.currentBlock.hasValidProof() == true) {
+		fmt.Printf("found proof for block ${this.currentBlock.chainLength}: ${this.currentBlock.proof}")
+        // this.log(`found proof for block ${this.currentBlock.chainLength}: ${this.currentBlock.proof}`);
+        m.announceProof();
+        m.receiveBlock(this.currentBlock);
+        m.startNewSearch();
+        break;
+	  }
+	}
+
+
+	
+
+
+	m.CurrentBlock.Proof++;
+	  
+    // If we are testing, don't continue the search.
+    if(oneAndDone == false) {
+      // Check if anyone has found a block, and then return to mining.
+      setTimeout(() => this.emit(Blockchain.START_MINING), 0);
+    }
+  }
